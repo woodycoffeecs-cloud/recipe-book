cd827e554f65614b3d29214f73a308cb77d5086d
woodycoffeecs-cloud <woody.coffee.cs@gmail.com>
2026-05-04 13:23:39 +0700

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// ⚠️ Điền thông tin Supabase của bạn vào đây
-// Lấy từ: Supabase Dashboard → Project Settings → API
 const SUPABASE_URL = 'https://hjwzsopwimndvdkykxfy.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_ZA8fOkh_7bfiqk5YkYAxPQ_1Vf17C0b'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhqd3pzb3B3aW1uZHZka3lreGZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NjYyODIsImV4cCI6MjA5MzQ0MjI4Mn0.vIGcRi6Rnvz39bVPAGy2ONnG9-0K4mg2JetiwepvY9w'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
